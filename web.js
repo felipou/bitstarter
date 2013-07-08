d94527a85253620f44e886464eef70ed813fac6f
Felipe Machado <felipou@gmail.com>
2013-07-08 15:15:09 -0300

    Reads index.html on root path

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var response_string = fs.readFileSync("index.html").toString();
+    response.send(response_string);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
